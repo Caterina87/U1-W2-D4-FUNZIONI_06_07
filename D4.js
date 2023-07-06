adb0129ada804235ca1c9295c5abb830846684cb
Caterina Munno <caterina.munno@gmail.com>
2023-07-06 16:00:05 +0200

    add excercise

--- D4.js
+++ D4.js
@@ -11,6 +11,13 @@ REGOLE
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+function area(l1, l2) {
+  const total = l1 * l2;
+  return total;
+}
+
+console.log("Primo esercizio", area(15, 20));
+
 /* ESERCIZIO 2
  Scrivi una funzione di nome "crazySum", che riceve due numeri interi come parametri.
  La funzione deve ritornare la somma dei due parametri, ma se il valore dei due parametri è il medesimo deve invece tornare
@@ -19,6 +26,19 @@ REGOLE
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+function crazySum(n1, n2) {
+  let total = 0;
+  if (n1 === n2) {
+    total = (n1 + n2) * 3;
+  } else {
+    total = n1 + n2;
+  }
+  return total;
+}
+
+console.log("Secondo esercizio - con numero diverso", crazySum(15, 10));
+console.log("Secondo esercizio -con numero uguale", crazySum(15, 15));
+
 /* ESERCIZIO 3
  Scrivi una funzione di nome "crazyDiff" che calcola la differenza assoluta tra un numero fornito come parametro e 19.
  Deve inoltre tornare la differenza assoluta moltiplicata per tre qualora il numero fornito sia maggiore di 19.
@@ -26,12 +46,35 @@ REGOLE
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+function crazyDiff(n) {
+  let total = Math.abs(n - 19);
+  if (n > 19) {
+    total = total * 3;
+  }
+  return total;
+}
+
+console.log("Terzo esercizio - con numero < di 19", crazyDiff(15));
+console.log("Terzo esercizio - con numero > di 19", crazyDiff(25));
+
 /* ESERCIZIO 4
- Scrivi una funzione di nome "boundary" che accetta un numero intero n come parametro, e ritorna true se n è compreso tra 20 e 100 (incluso) oppure
+ Scrivi una funzione di nome "boundary" che accetta un numero intero n come parametro, e ritorna true se n è
+  compreso tra 20 e 100 (incluso) oppure
  se n è uguale a 400.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+function boundary(n) {
+  let ret = "false";
+  if (n <= 100 || n === 400) {
+    ret = "true";
+  }
+  return ret;
+}
+
+console.log("Quarto esercizio - con true", boundary(90));
+console.log("Terzo esercizio - con false", boundary(255));
 
 /* ESERCIZIO 5
  Scrivi una funzione di nome "epify" che accetta una stringa come parametro.
@@ -42,7 +85,8 @@ REGOLE
 /* SCRIVI QUI LA TUA RISPOSTA */
 
 /* ESERCIZIO 6
- Scrivi una funzione di nome "check3and7" che accetta un numero positivo come parametro. La funzione deve controllare che il parametro sia un multiplo
+ Scrivi una funzione di nome "check3and7" che accetta un numero positivo come parametro. La funzione deve controllare
+che il parametro sia un multiplo
  di 3 o di 7. (Suggerimento: usa l'operatore modulo)
 */
 
